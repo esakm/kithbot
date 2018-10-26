61629b7296d0690434bd2a73f1c785eb3e881fbb
Momo <iamdissappoint@gmail.com>
2018-10-26 15:35:22 -0400

    Version 1

--- scripts/bot.py
+++ scripts/bot.py
@@ -49,17 +49,18 @@ def change_size(size, back_up_size=None):
                 option.click()
                 print("Main size found. ({})".format(str(size)))
                 return True
-        for option in options:
-            if isinstance(back_up_size, list):
-                if option.text in back_up_size:
-                    print("Back up size found. ({})".format(option.text))
-                    option.click()
-                    return True
-            elif isinstance(back_up_size, str):
-                if option.text == back_up_size:
-                    print("Back up size found. ({})".format(option.text))
-                    option.click()
-                    return True
+        if back_up_size:
+            for option in options:
+                if isinstance(back_up_size, list):
+                    if option.text in back_up_size:
+                        print("Back up size found. ({})".format(option.text))
+                        option.click()
+                        return True
+                elif isinstance(back_up_size, str):
+                    if option.text == back_up_size:
+                        print("Back up size found. ({})".format(option.text))
+                        option.click()
+                        return True
         print("Size not found. Going with default. ({})".format(driver.find_element_by_class_name("dk-selected ").text))
         return True
     except Exception as e:
@@ -118,6 +119,7 @@ def main():
     config = get_config_file()
     get_targets(config)
     launch_browser()
+    driver.implicitly_wait(30)
     print("Starting...")
     driver.get("https://kith.com")
     driver.add_cookie({"name": "KL_FORMS_MODAL", "value": "{%22disabledForms%22:{%22KyEV5m%22:{%22lastCloseTime%22:1540"
@@ -126,6 +128,7 @@ def main():
     print("-----------------------------------------------------------------------")
     num_added = 0
     num_targets = len(targets)
+    t1 = time.time()
     for target in targets:
         target_name = target['name']
         driver.get(get_search_query_url(target))
@@ -147,6 +150,7 @@ def main():
                 go_to_next_page(i + 1)
         print("-----------------------------------------------------------------------")
     print("All items processed. Checking out {}/{} items.".format(num_added, num_targets))
+    print("Time: %.2f" % (time.time() - t1))
     go_to_cart()
     go_to_checkout()
     time.sleep(500)
