a4e9c25e39da941962e5d526d6c36e1f162240e2
Momo <iamdissappoint@gmail.com>
2018-10-26 00:38:33 -0400

    Version 1

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='kithBot',
+   version='1.0',
+   description='Scrape kith.com',
+   author='Momo',
+   author_email='iamdissappoint@gmail.com',
+   packages=['scripts'],
+   install_requires=['selenium']
+)
