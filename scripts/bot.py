40c6f6f0bbbb8cc11b1343e9484a78a9878ec64a
Momo <iamdissappoint@gmail.com>
2018-10-26 02:47:32 -0400

    Version 1

--- scripts/bot.py
+++ scripts/bot.py
@@ -1,4 +1,4 @@
-import json, time, os
+import json, time
 from selenium import webdriver
 
 targets = []
@@ -22,19 +22,21 @@ char_encodings = {
 
 
 def get_config_file():
-    print(os.getcwd())
+    print("Loading config.")
     with open('config.json') as fd:
         return json.load(fd)
 
 
 def get_targets(config_data):
     global targets
+    print("Grabbing targets.")
     for target in config_data["targets"]:
         targets.append(target)
 
 
 def launch_browser():
     global driver
+    print("Launching browser")
     driver = webdriver.Chrome()
 
 
@@ -45,19 +47,23 @@ def change_size(size, back_up_size=None):
         for option in options:
             if option.text == str(size):
                 option.click()
+                print("Main size found. ({})".format(str(size)))
                 return True
         for option in options:
             if isinstance(back_up_size, list):
                 if option.text in back_up_size:
+                    print("Back up size found. ({})".format(option.text))
                     option.click()
                     return True
             elif isinstance(back_up_size, str):
                 if option.text == back_up_size:
+                    print("Back up size found. ({})".format(option.text))
                     option.click()
                     return True
-        return False
+        print("Size not found. Going with default. ({})".format(driver.find_element_by_class_name("dk-selected ").text))
+        return True
     except Exception as e:
-        print(e.args)
+        return False
 
 
 def replace_special_chars(item_name):
@@ -68,16 +74,10 @@ def replace_special_chars(item_name):
     return item_name
 
 
-def prepare_for_query(item_name):
-    item_name = replace_special_chars(item_name)
-    item_name = "-".join(item_name.rsplit())
-    return item_name
-
-
 def get_search_query_url(item):
     global search_url
     name = item["name"].lower()
-    name = prepare_for_query(name)
+    name = replace_special_chars(name)
     return search_url.format(name)
 
 
@@ -101,8 +101,9 @@ def go_to_next_page(page_number):
 def add_to_cart():
     try:
         driver.find_element_by_class_name("product-single-add-to-cart").click()
+        return True
     except Exception:
-        pass
+        return False
 
 def go_to_cart():
     global cart_url
@@ -117,20 +118,35 @@ def main():
     config = get_config_file()
     get_targets(config)
     launch_browser()
+    print("Starting...")
     driver.get("https://kith.com")
     driver.add_cookie({"name": "KL_FORMS_MODAL", "value": "{%22disabledForms%22:{%22KyEV5m%22:{%22lastCloseTime%22:1540"
                                                           "526200%2C%22successActionTypes%22:[]}}}", "domain": "kith.com"})
+
+    print("-----------------------------------------------------------------------")
+    num_added = 0
+    num_targets = len(targets)
     for target in targets:
         target_name = target['name']
         driver.get(get_search_query_url(target))
+        print("Current item: {}".format(target_name))
         for i in range(1, 6):
             if go_to_item_page(target_name):
-                change_size(target['size'])
+                print("Found {}, attempting to add to cart".format(target_name))
+                if not change_size(target['size']):
+                    print("Item sold out.")
+                    break
                 time.sleep(1)
-                add_to_cart()
+                if add_to_cart():
+                    num_added += 1
+                    print("Added {} to cart.".format(target_name))
+                else:
+                    print("Failed to add {} to the cart, probably sold out".format(target_name))
                 break
             else:
                 go_to_next_page(i + 1)
+        print("-----------------------------------------------------------------------")
+    print("All items processed. Checking out {}/{} items.".format(num_added, num_targets))
     go_to_cart()
     go_to_checkout()
     time.sleep(500)
